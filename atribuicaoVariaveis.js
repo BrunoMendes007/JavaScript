f8532dd17bbe0605fbe145a7c64037c1fbf056da
BrunoMendes007 <bruno.neduziak.mendes@escola.pr.gov.br>
2025-08-29 12:21:31 +0000

    lista

---
+++ atribuicaoVariaveis.js
@@ -0,0 +1,17 @@
+console.log("Trabalhando com atribuição de variáveis");
+
+const primeiroNome = "Bruno";
+const sobrenome = "Mendes";
+
+console.log("Meu nome é ${primeiroNome} ${sobrenome}");
+
+let contador = "0";
+contador = contador + 1;
+
+const nomeCompleto = primeiroNome + sobrenome;
+console.log(nomeCompleto);
+
+let; //declarando variáveis
+idade = 16; //atribuindo valor
+idade = idade + 1;
+console.log(idade);
